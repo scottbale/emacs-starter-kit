0f0aedfb0b3db16801a909f6874db9375744622d
Scott Bale <scott@balehaus.org>
2011-08-18 16:33:10 -0500

    use command key as meta key

--- init.el
+++ init.el
@@ -40,6 +40,11 @@
 ;; These should be loaded on startup rather than autoloaded on demand
 ;; since they are likely to be used in every session
 
+(setq mac-option-key-is-meta nil)
+(setq mac-command-key-is-meta t)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier nil)
+
 (require 'cl)
 (require 'saveplace)
 (require 'ffap)
