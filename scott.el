b73c02ed9b5cb3ed60ae9add34f2c5e0ac3a9e1e
Scott Bale <scott@balehaus.org>
2011-08-26 16:18:28 -0500

    add color-theme-6.6.0

--- scott.el
+++ scott.el
@@ -1,3 +1,13 @@
+;; Color Theme
+(setq dotfiles-dir (file-name-directory
+                    (or (buffer-file-name) load-file-name)))
+(setq user-specific-dir (concat dotfiles-dir user-login-name))
+(add-to-list 'load-path (concat user-specific-dir "/color-theme"))
+(require 'color-theme)
+(setq color-theme-is-global t)
+(color-theme-initialize)
+(color-theme-dark-blue2)
+
 (defun compile-load-withns ()
   "Compiles the clj file, loads it into the repl switches the current namespace of the repl to the one the file is in and then switches to the slime repl buffer"
   (interactive)
@@ -6,3 +16,4 @@
   (slime-switch-to-output-buffer))
 
 (add-hook 'clojure-mode-hook (lambda ()(define-key clojure-mode-map "\C-cd" 'compile-load-withns)))
+
